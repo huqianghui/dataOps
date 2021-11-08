e31ad09b926745d4487a2196e13365b0710b8e00
huqianghui <huqianghui0613@163.com>
2021-11-08 21:28:59 +0800

    Update: README.md azureSynapseAnalytics dataflow dataset factory linkedService notebooks pipeline

--- notebooks/Users/rosefinch@qianghuihu.partner.onmschina.cn/capNetworkLayersPacketRead.py
+++ notebooks/Users/rosefinch@qianghuihu.partner.onmschina.cn/capNetworkLayersPacketRead.py
@@ -33,7 +33,7 @@ lenght=len(allPackets)
 
 # COMMAND ----------
 
-lenght = 5
+lenght = 1
 
 for index in range(lenght):
     for layerNo in range(layers):
@@ -44,10 +44,48 @@ for index in range(lenght):
 
 # COMMAND ----------
 
-print(len(allPackets))
-# fieldName="src"
-# fieldValue = getattr(allPackets[0][1], fieldName)
-# print("fieldValue: " + str(fieldValue))
+# import pyspark class Row from module sql
+from pyspark.sql import *
+from pyspark.sql.type import *
+
+schema = StructType([StructField("mac_dst",StringType(),False),
+                    StructField("mac_src",StringType(),False),
+                    StructField("type",IntegerType(),False),
+                    StructField("ip_options",ArrayType(),True),
+                    StructField("version",IntegerType(),False),
+                    StructField("ihl",StringType(),True),
+                    StructField("tos",IntegerType(),False),
+                    StructField("len",IntegerType(),False),
+                    StructField("id",IntegerType(),False),
+                    StructField("ip_flags",StringType(),False),
+                    StructField("frag",IntegerType(),False),
+                    StructField("ttl",IntegerType(),False),
+                    StructField("proto",IntegerType(),False),
+                    StructField("ip_chksum",IntegerType(),True), 
+                    StructField("ip_src",StringType(),False), 
+                    StructField("ip_dst",StringType(),False), 
+                    StructField("sport",IntegerType(),False), 
+                    StructField("dport",IntegerType(),False),  
+                    StructField("seq",IntegerType(),False), 
+                    StructField("ack",IntegerType(),False),  
+                    StructField("dataofs",IntegerType(),False),  
+                    StructField("tcp_reserved",IntegerType(),False),  
+                    StructField("tcp_flags",StringType(),False),  
+                    StructField("window",IntegerType(),False),  
+                    StructField("tcp_chksum",IntegerType(),False),  
+                    StructField("urgptr",IntegerType(),False),  
+                    StructField("tcp_options",ArrayType(),True),   
+                    ])
+CapResult = Row("mac_dst", "mac_src", "type","ip_options","version" ,"ihl","tos","len","id","ip_flags","frag","ttl","proto","ip_chksum","ip_src","ip_dst","sport","dport","seq","ack","dataofs","reserved","tcp_reserved","window","tcp_chksum","urgptr","tcp_options")
+
+cap1=CapResult("2021-10-21 17:07:19.810486","172.16.6.187","172.16.12.166","40774","6380","TCP","66","40774 → 6380 [ACK] Seq=1 Ack=2 Win=283 Len=0 SLE=1 SRE=2")
+
+capResultSeq1=[cap1]
+capDataframe= spark.createDataFrame(capResultSeq1)
+
+capDataframe.show()
+
+capDataframe.write.format("delta").save("/mnt/delta/cap/") 
 
 # COMMAND ----------
 
